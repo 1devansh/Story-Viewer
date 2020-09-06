556625e1f842b7a5a798c15d482f95c53d62a380
Devansh Sharma <ds8611@bennett.edu.in>
2020-09-07 04:06:40 +0530

    static

--- my_stories/settings.py
+++ my_stories/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
